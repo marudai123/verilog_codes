7e25e0be476a7fe83589eb68c4bd2a99527331be
Marudai M <muru3892@gmail.com>
2026-08-26 18:12:45 +0530

    nor_gate

---
+++ nor_gate/nor_gate.v
@@ -0,0 +1,65 @@
+`timescale 1s / 100ms
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.08.2026 15:32:16
+// Design Name: 
+// Module Name: not_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nor_gate (a,b,y);
+    
+    input a;
+    input b;
+    output y;
+    
+    assign y = ~(a|b);
+    
+endmodule 
+
+
+module testbench;
+    
+    reg a;
+    reg b;
+    wire y;
+    
+    nor_gate  uut(
+    .a(a),
+    .b(b),
+    .y(y)
+    );
+    
+    initial
+     
+        begin
+         
+         a=0;b=0;
+         #25;
+         
+         b=1;
+         #25;
+         
+         a=1;b=0;
+         #25;
+         
+         b=1;
+         #25;
+         
+         $finish;
+        
+        end
+endmodule
